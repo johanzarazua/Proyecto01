bd6d59a2b3ccec6f215b5ac062f70797f5a7c099
Johan <johanzarazua@gmail.com>
2021-07-17 22:47:01 -0500

    Initial commit

---
+++ app/src/main/java/com/johanzr/proyecto01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.johanzr.proyecto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}
